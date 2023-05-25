c6b47b8c52c6e62d797dad27f35f71adc13c4236
Aligan18 <alisherganiev18@gmail.com>
2023-05-25 19:41:11 +0600

    I added the  react-router-dom  and lazy loading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Counter from '../components/Counter'
+import React, { Suspense } from 'react'
+import { Link, Route, Router, Routes } from 'react-router-dom'
+import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async'
+import { HomePageAsync } from '../pages/HomePage/HomePage.async'
 import './index.scss'
 
 const App = () => {
   return (
-    <div className="app">
-        Hello World
-        <Counter/>
+    <div className="app"> 
+          <Link to={'/'}>Home page </Link>
+          <Link to={'/about'}>About page </Link>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route path="/" element={<HomePageAsync/>}/>
+                <Route path="/about" element={<AboutPageAsync/>}/>
+            </Routes>
+          </Suspense>
     </div>
   )
 }
