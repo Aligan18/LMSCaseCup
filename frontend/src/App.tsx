288a7f5565b55a75c8c6116c92efada816a12eb3
Aligan18 <alisherganiev18@gmail.com>
2023-05-26 09:33:12 +0600

    I added the global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { Link, Route, Router, Routes } from 'react-router-dom'
-import { AboutPageAsync } from '../pages/AboutPage/AboutPage.async'
-import { HomePageAsync } from '../pages/HomePage/HomePage.async'
-import './index.scss'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { HomePageAsync } from './pages/HomePage/HomePage.async'
+import './styles/index.scss'
+
+enum ETheme {
+  DARK = 'dark',
+  LIGHT = 'light'
+}
 
 const App = () => {
+  const [theme , setTheme ] = useState<ETheme>(ETheme.LIGHT)
+
+  const handleTheme =()=>{
+    const changedTheme = theme === ETheme.DARK ? ETheme.LIGHT : ETheme.DARK
+    console.log(changedTheme)
+    setTheme(changedTheme)
+  }
+  
   return (
-    <div className="app"> 
+    <div className={`app ${theme}`}>  
+          <button onClick={handleTheme}>Theme</button>
           <Link to={'/'}>Home page </Link>
           <Link to={'/about'}>About page </Link>
           <Suspense fallback={<div>Loading...</div>}>
