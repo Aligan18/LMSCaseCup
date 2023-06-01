3470fa9d8408dfa7c811db1318e47a0713f9d213
Aligan18 <alisherganiev18@gmail.com>
2023-06-01 16:41:18 +0600

    I added the Webpack hot replacement

--- frontend/config/build/BuildPlugins.ts
+++ frontend/config/build/BuildPlugins.ts
@@ -1,5 +1,7 @@
 import HtmlWebpackPlugin from "html-webpack-plugin"
 import  MiniCssExtractPlugin from "mini-css-extract-plugin"
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin"
+
 
 import webpack  from "webpack"
 import { IBuildOptions, IBuildPath } from "./types/config"
@@ -18,6 +20,8 @@ export function buildPlugins({paths,isDev }:IBuildOptions) : webpack.WebpackPlug
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        new ReactRefreshWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin()
       ]
 }
