ddce5fb57970a4d4cb1cc4ad4fb8d96dec2ac5b5
Thinknov <shirin.evgeniy12@gmail.com>
2023-06-04 13:01:28 +0600

    edit models 'admins' and 'certificates'

--- backend/lmssite/certificates/models.py
+++ backend/lmssite/certificates/models.py
@@ -1,6 +1,13 @@
+from datetime import date
 from django.db import models
 
 class Certificates(models.Model):
+    course = models.ForeignKey("Course", on_delete=models.PROTECT, null=True)
+    student = models.ManyToManyField("Students")
+    name = models.CharField(max_length=255)
+    data = models.DateField(default=date.today)
+    file = models.FileField(upload_to="uploads/")
+
 
     def __str__(self):
-        return self.title
+        return self.name
