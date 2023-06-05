9d11ad19d23d0280d958398b8301f56538c7074c
Thinknov <shirin.evgeniy12@gmail.com>
2023-06-05 11:11:04 +0600

    edit admins, certificates and comments models

--- backend/lmssite/admins/models.py
+++ backend/lmssite/admins/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Admins(models.Model):
-    admin_type = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-    patronymic = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
+    ADMIN_TYPES = [
+        ("1", "Admin"),
+        ("2", "Super Admin")
+    ]
+    admin_type = models.CharField(choices=ADMIN_TYPES, default="1", max_length=1)
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40)
+    patronymic = models.CharField(max_length=40)
+    email = models.EmailField(max_length=40)
 
     def __str__(self):
         return self.admin_type
