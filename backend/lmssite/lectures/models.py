29001df4b609a400be71d4a05dc249677004366b
Thinknov <shirin.evgeniy12@gmail.com>
2023-06-06 10:50:12 +0600

    edit lectures, list_modules and students models

--- backend/lmssite/lectures/models.py
+++ backend/lmssite/lectures/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Lectures(models.Model):
+    title = models.CharField(max_length=150, null=True)
+    content = models.TextField(blank=True)
+
 
     def __str__(self):
         return self.title
