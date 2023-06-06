a29d162205da0127e4a810abea1272ab88f84895
Aligan18 <alisherganiev18@gmail.com>
2023-06-06 22:25:18 +0600

    I added views

--- backend/lmssite/list_modules/views.py
+++ backend/lmssite/list_modules/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+
+from list_modules.models import ListModules
+from list_modules.serializers import ListModulesSerializers, CreateListModulesSerializers
+
+
+class ListModulesView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ListModules.objects.all()
+    serializer_class = ListModulesSerializers
+
+
+class AllCreateListModulesView(generics.ListCreateAPIView):
+    queryset = ListModules.objects.all()
+    serializer_class = CreateListModulesSerializers
+
+
--- backend/lmssite/students/views.py
+++ backend/lmssite/students/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+
+from students.models import Students
+from students.serializers import StudentsSerializers, CreateStudentsSerializers, AboutStudentsSerializers
+
+
+class StudentsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Students.objects.all()
+    serializer_class = StudentsSerializers
+
+
+class CreateStudentsView(generics.CreateAPIView):
+    queryset = Students.objects.all()
+    serializer_class = CreateStudentsSerializers
+
+
+class AllStudentsView(generics.CreateAPIView):
+    queryset = Students.objects.all()
+    serializer_class = AboutStudentsSerializers
