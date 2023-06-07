95d86c0ef1680fc3e89a281fa1bd4cca7bb6aed4
Aligan18 <alisherganiev18@gmail.com>
2023-06-07 20:44:24 +0600

    I added signals to create user profile

--- backend/lmssite/students/models.py
+++ backend/lmssite/students/models.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Students(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     courses = models.ManyToManyField("course.Course")
     name = models.CharField(max_length=40, null=True)
     surname = models.CharField(max_length=40, null=True)
     patronymic = models.CharField(max_length=40, null=True)
-    email = models.EmailField(max_length=60, null=True)
     about = models.TextField(blank=True, null=True)
 
     def __str__(self):
--- backend/lmssite/teachers/models.py
+++ backend/lmssite/teachers/models.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Teachers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=40, null=True)
     surname = models.CharField(max_length=40, null=True)
     patronymic = models.CharField(max_length=40, null=True)
-    email = models.EmailField(max_length=60, null=True)
     about = models.TextField(blank=True, null=True)
     category = models.ForeignKey("categories.Category", on_delete=models.PROTECT, null=True)
 
