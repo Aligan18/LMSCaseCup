abcf1e27afbe40adbbf4ace560b883deefd0a0ea
Aligan18 <alisherganiev18@gmail.com>
2023-06-10 00:25:25 +0600

    I fixed bags

--- backend/lmssite/certificates/views.py
+++ backend/lmssite/certificates/views.py
@@ -3,16 +3,15 @@ from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 
 from .models import Certificates
-from .permissions import IsTeacher
+
 from .serializers import CertificatesSerializers, CreateCertificatesSerializers
 
 
 class CertificatesCreateView(generics.CreateAPIView):
     queryset = Certificates.objects.all()
     serializer_class = CreateCertificatesSerializers
-    permission_classes = (IsTeacher, )
+
 
 class CertificatesListView(generics.ListAPIView):
     queryset = Certificates.objects.all()
     serializer_class = CertificatesSerializers
-    permission_classes = (IsAdminUser, IsTeacher,)
--- backend/lmssite/course/views.py
+++ backend/lmssite/course/views.py
@@ -8,43 +8,41 @@ from course.serializers import CreateCourseSerializers, AboutCourseSerializers, 
     DeleteCourseSerializers, UpdateCourseSerializers
 from django_filters.rest_framework import DjangoFilterBackend
 
-<<<<<<< HEAD
 from .permissions import IsTeacherAdmin, IsOwnerTeacherAdmin
-=======
+
 from mysite import permissions
->>>>>>> 62245e01de1c050429bab49faf9995b95f4bd042
+
 from .service import CategoryFilter
 
 
 class CourseCreateView(generics.CreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CreateCourseSerializers
-<<<<<<< HEAD
+
     permission_classes = [IsTeacherAdmin]
 
     def perform_create(self, serializer):
         serializer.validated_data['teacher'] = self.request.user
         serializer.save()
 
-=======
     permission_classes = (IsAdminUser,)
+
 
 class CourseDeleteView(generics.DestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = DeleteCourseSerializers
     permission_classes = (IsAdminUser,)
->>>>>>> 62245e01de1c050429bab49faf9995b95f4bd042
+
 
 class CourseUpdateView(generics.UpdateAPIView):
     queryset = Course.objects.all()
-<<<<<<< HEAD
+
     serializer_class = CreateCourseSerializers
     permission_classes = [IsOwnerTeacherAdmin]
 
-=======
     serializer_class = UpdateCourseSerializers
     permission_classes = (permissions.IsAdminTeacher1,)
->>>>>>> 62245e01de1c050429bab49faf9995b95f4bd042
+
 
 class CourseListView(generics.ListAPIView):
     queryset = Course.objects.all()
@@ -53,10 +51,7 @@ class CourseListView(generics.ListAPIView):
     filterset_class = CategoryFilter
     permission_classes = (AllowAny,)
 
-<<<<<<< HEAD
 
 class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CategorySerializers
-=======
->>>>>>> 62245e01de1c050429bab49faf9995b95f4bd042
