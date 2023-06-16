227649afcef4d69474f44c055970de085717ae91
Thinknov <shirin.evgeniy12@gmail.com>
2023-06-16 18:35:32 +0600

    added grades filters

--- backend/lmssite/grades/views.py
+++ backend/lmssite/grades/views.py
@@ -1,37 +1,41 @@
 # grades с фильтрацией по course  , для просмотра  всех оценок студентов
-
-
+from django_filters.rest_framework import DjangoFilterBackend
 # grades с фильтрацией по course и студенту   , для просмотра   всех оценок определенного студента
 
 
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
 
-from custom_user.permissions import IsStudent, IsTeacherHasAccessCreate, IsTeacherHasAccess
+from custom_user.permissions import IsStudent, IsTeacherHasAccessCreate, IsTeacherHasAccess, IsStudentOwner
 from grades.models import Grades
 from grades.serializers import GradesSerializers, CreateGradesSerializers, AboutGradesSerializers
+from grades.service import Filter, FilterOnlyCourse
 
 
 # Student and Admin
-class AttendanceForLecturesViewCreate(generics.CreateAPIView):
+class AttendanceForLecturesViewCreate(generics.CreateAPIView): #?????????
     queryset = Grades.objects.all()
     serializer_class = CreateGradesSerializers
     permission_classes = [IsAdminUser | IsStudent]
 
 
-# нужно передовать /?course=<id>
+# нужно передавать /?course=<id>
 # Admin , Teacher имеющий доступ , Student свои оценки
-class GradesViewList(generics.ListAPIView): # grades с фильтрацией по course и студенту   , для просмотра   всех оценок определенного студента
+class GradesOneStudentViewList(generics.ListAPIView): # grades с фильтрацией по course и студенту   , для просмотра   всех оценок определенного студента
     queryset = Grades.objects.all()
     serializer_class = AboutGradesSerializers
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = Filter
     permission_classes = [IsAdminUser | IsTeacherHasAccessCreate]
 
 
-# нужно передовать /?course=<id>
+# нужно передавать /?course=<id>
 # Admin , Teacher имеющий доступ
 class GradesViewList(generics.ListAPIView): # grades с фильтрацией по course  , для просмотра  всех оценок студентов
     queryset = Grades.objects.all()
     serializer_class = AboutGradesSerializers
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = FilterOnlyCourse
     permission_classes = [IsAdminUser | IsTeacherHasAccessCreate]
 
 
