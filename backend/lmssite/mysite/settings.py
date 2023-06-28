412eb895b7d00266c71078c0cff3dbb95e42e814
Aligan18 <alisherganiev18@gmail.com>
2023-06-28 21:33:15 +0600

    Test

--- backend/lmssite/mysite/settings.py
+++ backend/lmssite/mysite/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 ]
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # TEMPLATES = [
