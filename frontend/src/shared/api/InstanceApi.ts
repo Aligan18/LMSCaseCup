a676c846ccd4320fe235ca16b2a95876a36186c3
Aligan18 <alisherganiev18@gmail.com>
2023-07-19 09:38:37 +0600

    I fix bugs

--- frontend/src/shared/api/InstanceApi.ts
+++ frontend/src/shared/api/InstanceApi.ts
@@ -10,8 +10,6 @@ const host_url = process.env.REACT_APP_API_URL + version
 const token: IToken = JSON.parse(String(localStorage.getItem(TOKEN_LOCALSTORAGE_KEY)))
 // const token = user?.token.access
 
-console.log('token', token.access)
-
 export const $api = axios.create({
 	baseURL: host_url,
 	headers: { Authorization: `Bearer ${token?.access}` },
