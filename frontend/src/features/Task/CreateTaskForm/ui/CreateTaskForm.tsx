8fcacba2b26ffe9c8262136c0ef85c0f08e7d26a
Thinknov <shirin.evgeniy12@gmail.com>
2023-08-01 09:02:10 +0600

    edded create task page

---
+++ frontend/src/features/Task/CreateTaskForm/ui/CreateTaskForm.tsx
@@ -0,0 +1,62 @@
+import { SubmitHandler } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+
+import classes from './CreateTaskForm.module.scss'
+
+import { ICreateTaskData, ITaskFormConstructor } from 'entities/Task/types/Task.types'
+
+import { classnames as cn } from 'shared/lib'
+import { FormConstructor } from 'shared/ui'
+
+export const CreateTaskForm = ({ styles }: ICreateTaskFormProps) => {
+	const { t } = useTranslation('course')
+
+	const onSubmit: SubmitHandler<ICreateTaskData> = (formData: ICreateTaskData, event) => {
+		event?.preventDefault()
+
+		console.log(formData)
+	}
+
+	const data: ITaskFormConstructor[] = [
+		{
+			type: 'input',
+			title: `${t('nazvanie-zadaniya')}`,
+			description: `${t('do')}` + 200 + `${t('simvolov')}`,
+			key: 'title',
+			rules: {
+				required: true,
+				maxLength: 200,
+			},
+		},
+		{
+			type: 'text-input',
+			title: `${t('zadanie')}`,
+			key: 'description',
+			rules: {
+				required: true,
+			},
+		},
+		{
+			title: `${t('fail')}`,
+			type: 'file-input',
+			key: 'file',
+		},
+	]
+
+	return (
+		<div className={cn(classes.CreateTaskForm, [styles])}>
+			<div className={classes.wrapper}>
+				<FormConstructor<ICreateTaskData>
+					onSubmit={onSubmit}
+					data={data}
+					button={`${t('sokhranit-0')}`}
+					styles={classes.form}
+				/>
+			</div>
+		</div>
+	)
+}
+
+interface ICreateTaskFormProps {
+	styles?: string
+}
