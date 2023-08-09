baf92a5990b7e9b3f9f5a1977bbf150df3e7d5b0
Aligan18 <alisherganiev18@gmail.com>
2023-08-09 12:38:07 +0600

    I added prod url

--- frontend/src/shared/api/InstanceApi.ts
+++ frontend/src/shared/api/InstanceApi.ts
@@ -5,10 +5,11 @@ import { IToken } from 'entities/Authorization/types'
 import { TOKEN_LOCALSTORAGE_KEY } from 'shared/const'
 
 const version = '/api/v1'
-const host_url = process.env.REACT_APP_API_URL + version
+const host_url = __IS_DEV__ ? process.env.REACT_APP_API_URL + version : process.env.REACT_APP_API_PROD + version
 const token: IToken = JSON.parse(String(localStorage.getItem(TOKEN_LOCALSTORAGE_KEY)))
 // const token = user?.token.access
 
+console.log('host_url', host_url)
 export let $api: AxiosInstance
 
 if (token?.access) {
