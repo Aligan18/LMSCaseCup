b005f50bcf423d9fa6ece1d251a4871b84daad45
Aligan18 <alisherganiev18@gmail.com>
2023-08-10 21:12:16 +0600

    I fixed token auth

--- frontend/src/shared/api/InstanceApi.ts
+++ frontend/src/shared/api/InstanceApi.ts
@@ -1,14 +1,16 @@
 import axios, { AxiosInstance } from 'axios'
+import { useSelector } from 'react-redux'
 
 import { IToken } from 'entities/Authorization/types'
+import { getUserToken } from 'entities/Users/CustomUser'
 
 import { TOKEN_LOCALSTORAGE_KEY } from 'shared/const'
 
 const version = '/api/v1'
 const host_url = __IS_DEV__ ? process.env.REACT_APP_API_URL + version : process.env.REACT_APP_API_PROD + version
-const token: IToken = JSON.parse(String(localStorage.getItem(TOKEN_LOCALSTORAGE_KEY)))
+// const token: IToken = JSON.parse(String(localStorage.getItem(TOKEN_LOCALSTORAGE_KEY)))
 // const token = user?.token.access
-
+const token = useSelector(getUserToken)
 export let $api: AxiosInstance
 axios.defaults.withCredentials = true
 if (token?.access) {
